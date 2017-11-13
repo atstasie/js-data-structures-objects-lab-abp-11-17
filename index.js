95e794dc629e4d0f1036fa987c6aa3f0448a2070
Anastassia Troitskaia <atroitskaia@gmail.com>
2017-11-13 02:46:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ function deleteFromDriverByKey(driver, key) {
   return (newDriver = delete driver.key);
 }
 
-
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    return (delete driver.key);
+}
